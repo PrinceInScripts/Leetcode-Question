0619d333fe55752e4ad25af6dc3bb7db979215a9
princekumar <princepk7878kumar@gmail.com>
2024-04-11 00:55:32 +0530

    done 3 sum

--- 2. April 2024/Problem 4 {3 Sum}/Question4.cpp
+++ 2. April 2024/Problem 4 {3 Sum}/Question4.cpp
@@ -45,45 +45,42 @@ using namespace std;
 class Solution {
 public:
       
-      bool isExist(vector<vector<int>> nums,vector<int> a){
-        for(int i=0;i<nums.size();i++){
-            vector<int> arr={nums[i]};
-            int count=0;
-            bool flag=false;
-            for(int j=0;j<a.size();j++){
-                if(arr[j]==a[j]){
-                   count++;
-                }
-            }
-
-            if(count==a.size()){
-                flag=true;
-            }
-
-            return flag;
-        }
-      }
+     
       vector<vector<int>> threeSum(vector<int>& nums) {
         int n=nums.size();
-       vector<vector<int>> result;
-       sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++){
-           
-            for(int j=i+1;j<n;j++){
-               for(int k=j+1;k<n;k++){
-                if(nums[i]+nums[j]+nums[k]==0){
-                     vector<int> a;
-                    a.push_back(nums[i]);
-                    a.push_back(nums[j]);
-                    a.push_back(nums[k]);
-                    if(!isExist(result,a)){
-                       result.push_back(a);
-                    }
-                }
-               }
+        vector<vector<int>> res;
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<n-2;i++){
+            if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+
+            int target=-nums[i];
+            int j=i+1;
+            int k=n-1;
+
+            while(j<k){
+              int sum=nums[j]+nums[k];
+              if(sum==target){
+                res.push_back({nums[i],nums[j],nums[k]});
+                while (j < k && nums[j] == nums[j + 1])
+                    j++;
+                while (j < k && nums[k] == nums[k - 1])
+                    k--;
+                 j++;
+                 k--;
+              } else if(sum<target){
+                j++;
+              } else {
+                k--;
+              }
+
+             
             }
+
+           
         }
-        return result;
+         return res;
     }
 };
 
