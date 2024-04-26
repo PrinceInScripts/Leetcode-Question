61b216ed2c1f63e3a5225c99b4e27c5a1496fdff
princekumar <princepk7878kumar@gmail.com>
2024-04-26 23:38:51 +0530

    done

--- 2. April 2024/Problem 26 {Two Sum}/Question.cpp
+++ 2. April 2024/Problem 26 {Two Sum}/Question.cpp
@@ -45,14 +45,18 @@ class Solution
 {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-      int n=nums.size();
+        int n=nums.size();
+        vector<int> v;
       for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
             if(nums[i]+nums[j]==target){
-            return {i,j};
+            v.push_back(i);
+            v.push_back(j);
             }
         }
       }
+      return v;
+     
     }
 
 };
