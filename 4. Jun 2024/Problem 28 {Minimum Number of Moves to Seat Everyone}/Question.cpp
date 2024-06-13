68cb198164678745c23ff5bc7c680ca34886d9f4
princekumar <princepk7878kumar@gmail.com>
2024-06-13 16:56:20 +0530

    done

--- 4. Jun 2024/Problem 28 {Minimum Number of Moves to Seat Everyone}/Question.cpp
+++ 4. Jun 2024/Problem 28 {Minimum Number of Moves to Seat Everyone}/Question.cpp
@@ -59,7 +59,7 @@ class Solution
 {
 public:
       
-  int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+  int minMovesToSeat1(vector<int>& seats, vector<int>& students) {
         int n=seats.size();
         sort(seats.begin(),seats.end());
         sort(students.begin(),students.end());
@@ -72,6 +72,19 @@ public:
             }
         }
         return count;
+       
+    }
+  int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+         sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+        
+        int total_moves = 0;
+        
+        for (int i = 0; i < seats.size(); ++i) {
+            total_moves += abs(seats[i] - students[i]);
+        }
+        
+        return total_moves;
        
     }
 
