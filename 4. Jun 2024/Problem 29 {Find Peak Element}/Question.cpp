ad2e163cca12a660209a20ce1eb99572f63535f8
princekumar <princepk7878kumar@gmail.com>
2024-06-15 15:21:37 +0530

    done

--- 4. Jun 2024/Problem 29 {Find Peak Element}/Question.cpp
+++ 4. Jun 2024/Problem 29 {Find Peak Element}/Question.cpp
@@ -44,18 +44,14 @@ public:
         int lo=0;
         int hi=n-1;
 
-        while(lo<=hi){
+        while(lo<hi){
             int mid=lo+(hi-lo)/2;
-            if(n==1) return 0;
-            else if(n==2){
-                if(nums[0]>nums[1]) return 0;
-                else return 1;
-            }
-            else if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
-            else if(nums[mid]>nums[mid-1] && nums[mid]<nums[mid+1]) lo=mid+1;
-            else hi=mid-1;
+            
+            if(nums[mid]<nums[mid+1]) lo=mid+1;
+            else hi=mid;
         }
-        return -1;
+        return lo;
+       
        
 }
 
@@ -66,7 +62,7 @@ public:
 
 int main()
 {
-    vector<int> nums={2,1};
+    vector<int> nums={6,5,4,3,2,3,2};
 
     Solution solution;
     int ans = solution.findPeakElement(nums);
