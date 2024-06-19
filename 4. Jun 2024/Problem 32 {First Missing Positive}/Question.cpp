8280d4b0dd0856dbd8c6e89f5f3a26a3fe4b06ff
princekumar <princepk7878kumar@gmail.com>
2024-06-19 12:05:53 +0530

    done

--- 4. Jun 2024/Problem 32 {First Missing Positive}/Question.cpp
+++ 4. Jun 2024/Problem 32 {First Missing Positive}/Question.cpp
@@ -41,17 +41,29 @@ public:
  
    int firstMissingPositive(vector<int>& nums) {
         int n=nums.size();
-        
         int i=0;
         while(i<n){
-            if(nums[i]<=0 || nums[i]<n || nums[i]==i+1 || nums[nums[i]-1]==nums[i]) i++;
+            if(nums[i]<=0 || nums[i]==i+1) i++;
             else swap(nums[i],nums[nums[i]-1]);
         }
         for(int i=0;i<n;i++){
-           if(nums[i]!=i+1) return i+1;
+            if(nums[i]!=i+1) return i+1;
         }
         return -1;
 }
+//    int firstMissingPositive(vector<int>& nums) {
+//         int n=nums.size();
+        
+//         int i=0;
+//         while(i<n){
+//             if(nums[i]<=0 || nums[i]<n || nums[i]==i+1 || nums[nums[i]-1]==nums[i]) i++;
+//             else swap(nums[i],nums[nums[i]-1]);
+//         }
+//         for(int i=0;i<n;i++){
+//            if(nums[i]!=i+1) return i+1;
+//         }
+//         return -1;
+// }
 
 
    
