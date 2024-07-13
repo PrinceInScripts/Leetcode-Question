bd062c186dd996b40dddb070b5535a1503ac6081
princekumar <princepk7878kumar@gmail.com>
2024-07-13 13:30:17 +0530

    done

--- 5. July 2024/Problem 37 {Continuous Subarray Sum}/Question.cpp
+++ 5. July 2024/Problem 37 {Continuous Subarray Sum}/Question.cpp
@@ -58,7 +58,7 @@ class Solution {
 public:   
      
      
-     bool checkSubarraySum(vector<int>& nums, int k) {
+     bool checkSubarraySum1(vector<int>& nums, int k) {
         int n=nums.size();
 
        for(int i=1;i<n;i++){
@@ -88,48 +88,29 @@ public:
        return false;
 
     } 
-     bool checkSubarraySum1(vector<int>& nums, int k) {
-       int n=nums.size();
-
-       for(int i=1;i<n;i++){
-        nums[i]+=nums[i-1];
-       }
-       for(auto el:nums){
-        cout<<el<<" ";
-       }
-       cout<<endl;
-       int i=1,j=1;
-       while(j<n){
-          if(nums[j]%k==0){
-            cout<<"hi";
-             return true;
-          }
-          else if((nums[j]-nums[i-1])%k==0) {
-            cout<<j<<" "<<i<<endl;
-             cout<<nums[j]<<" "<<nums[i-1]<<endl;
-             cout<<"hi1"<<endl;
-             return true;
-          }
-          j++;
-          if(j==n-1 && i<n-1){
-            while(i<n-2){
-                if((nums[j]-nums[i-1])%k==0){
-                     cout<<"hi2";
-             return true;
-                }
-                i++;
-            }
-          }
-       }
-       return false;
+     bool checkSubarraySum(vector<int>& nums, int k) {
+      unordered_map<int,int> map;
+      map[0]=-1;
+      int sum=0;
+      for(int i=0;i<nums.size();i++){
+        sum+=nums[i];
+        if(map.find(sum%k)!=map.end()){
+           if(i-map[sum%k]>=2) return true;
+        } else {
+            map[sum%k]=i;
+        }
+      }
+      return false;
 
     } 
+    
+   
       
 };
 
 
 int main(){
-   vector<int> nums={};
+   vector<int> nums={23,2,4,6,7};
    int k=6;
    Solution solution; 
    int ans= solution.checkSubarraySum(nums,k);
