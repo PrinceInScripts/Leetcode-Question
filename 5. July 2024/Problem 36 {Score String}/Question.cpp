a6542b972439f46474d5c73f0eca58713ba65dad
princekumar <princepk7878kumar@gmail.com>
2024-07-13 08:07:56 +0530

    done

---
+++ 5. July 2024/Problem 36 {Score String}/Question.cpp
@@ -0,0 +1,81 @@
+
+
+/*
+# 3110. Score a String
+*/
+/*
+You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+
+Return the score of s.
+
+ 
+
+Example 1:
+
+Input: s = "hello"
+
+Output: 13
+
+Explanation:
+
+The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+
+Example 2:
+
+Input: s = "zaz"
+
+Output: 50
+
+Explanation:
+
+The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+
+ 
+
+Constraints:
+
+2 <= s.length <= 100
+s consists only of lowercase English letters.
+
+*/
+
+
+#include<iostream>
+#include<vector>
+#include<limits.h>
+#include<algorithm>
+#include<cmath>
+#include<unordered_map>
+#include<unordered_set>
+#include<sstream>
+#include<regex>
+using namespace std;
+
+class Solution {
+public:   
+     
+     
+     int scoreOfString(string s) {
+       int n=s.size();
+       int score=0;
+
+       for(int i=1;i<n;i++){
+          int diff=abs((int)s[i]-(int)s[i-1]);
+          score+=diff;
+       }
+       return score;
+
+    } 
+      
+};
+
+
+int main(){
+ string s="hello";
+ 
+   Solution solution; 
+   int ans= solution.scoreOfString(s);
+
+    cout<<ans;
+    return 0;
+}
