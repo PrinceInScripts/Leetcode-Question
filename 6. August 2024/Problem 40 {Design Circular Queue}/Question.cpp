71249dd1d8fd55e9fcf4d9efe3af1dd1aa8c0ffa
princekumar <princepk7878kumar@gmail.com>
2024-08-16 23:04:05 +0530

    done

---
+++ 6. August 2024/Problem 40 {Design Circular Queue}/Question.cpp
@@ -0,0 +1,124 @@
+
+
+/*
+# 622. Design Circular Queue
+*/
+/*
+Design your implementation of the circular queue. The circular queue is a linear data structure in which the operations are performed based on FIFO (First In First Out) principle, and the last position is connected back to the first position to make a circle. It is also called "Ring Buffer".
+
+One of the benefits of the circular queue is that we can make use of the spaces in front of the queue. In a normal queue, once the queue becomes full, we cannot insert the next element even if there is a space in front of the queue. But using the circular queue, we can use the space to store new values.
+
+Implement the MyCircularQueue class:
+
+MyCircularQueue(k) Initializes the object with the size of the queue to be k.
+int Front() Gets the front item from the queue. If the queue is empty, return -1.
+int Rear() Gets the last item from the queue. If the queue is empty, return -1.
+boolean enQueue(int value) Inserts an element into the circular queue. Return true if the operation is successful.
+boolean deQueue() Deletes an element from the circular queue. Return true if the operation is successful.
+boolean isEmpty() Checks whether the circular queue is empty or not.
+boolean isFull() Checks whether the circular queue is full or not.
+You must solve the problem without using the built-in queue data structure in your programming language. 
+
+ 
+
+Example 1:
+
+Input
+["MyCircularQueue", "enQueue", "enQueue", "enQueue", "enQueue", "Rear", "isFull", "deQueue", "enQueue", "Rear"]
+[[3], [1], [2], [3], [4], [], [], [], [4], []]
+Output
+[null, true, true, true, false, 3, true, true, true, 4]
+
+Explanation
+MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+myCircularQueue.enQueue(1); // return True
+myCircularQueue.enQueue(2); // return True
+myCircularQueue.enQueue(3); // return True
+myCircularQueue.enQueue(4); // return False
+myCircularQueue.Rear();     // return 3
+myCircularQueue.isFull();   // return True
+myCircularQueue.deQueue();  // return True
+myCircularQueue.enQueue(4); // return True
+myCircularQueue.Rear();     // return 4
+ 
+
+Constraints:
+
+1 <= k <= 1000
+0 <= value <= 1000
+At most 3000 calls will be made to enQueue, deQueue, Front, Rear, isEmpty, and isFull.
+*/
+
+
+#include<iostream>
+#include<vector>
+#include<limits.h>
+#include<algorithm>
+#include<cmath>
+#include<unordered_map>
+#include<unordered_set>
+#include<sstream>
+#include<stack>
+#include<string>
+using namespace std;
+class MyCircularQueue {
+public:
+    int f;
+    int b;
+    int s;
+    int c;
+    vector<int> arr;
+    MyCircularQueue(int k) {
+        f=0;
+        b=0;
+        s=0;
+        c=k;
+        vector<int> a(k);
+        arr=a;
+    }
+    
+    bool enQueue(int value) {
+        if(s==c) return false;
+        arr[b]=value;
+        b++;
+        if(b==c) b=0;
+        s++;
+        return true;
+    }
+    
+    bool deQueue() {
+        if(s==0) return false;
+        f++;
+        if(f==c) f=0;
+        s--;
+        return true;
+    }
+    
+    int Front() {
+        if(s==0) return -1;
+        return arr[f];
+    }
+    
+    int Rear() {
+        if(s==0) return -1;
+        if(b==0) return arr[c-1];
+        return arr[b-1];
+    }
+    
+    bool isEmpty() {
+        if(s==0) return true;
+        else return true;
+    }
+    
+    bool isFull() {
+        if(s==c) return true;
+        else return false;
+    }
+};
+
+
+
+int main(){
+   
+
+}
