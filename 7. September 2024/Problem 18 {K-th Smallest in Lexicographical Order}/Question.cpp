6540cd4e2f57125c2ca73f4f1f04b10a0b447a9b
princekumar <princepk7878kumar@gmail.com>
2024-09-22 20:44:28 +0530

    done

---
+++ 7. September 2024/Problem 18 {K-th Smallest in Lexicographical Order}/Question.cpp
@@ -0,0 +1,76 @@
+
+
+/*
+# 440.K-th Smallest in Lexicographical Order
+*/
+/*
+Given two integers n and k, return the kth lexicographically smallest integer in the range [1, n].
+
+ 
+
+Example 1:
+
+Input: n = 13, k = 2
+Output: 10
+Explanation: The lexicographical order is [1, 10, 11, 12, 13, 2, 3, 4, 5, 6, 7, 8, 9], so the second smallest number is 10.
+Example 2:
+
+Input: n = 1, k = 1
+Output: 1
+ 
+
+Constraints:
+
+1 <= k <= n <= 109
+ 
+*/
+
+#include <iostream>
+#include <vector>
+#include<algorithm>
+#include<unordered_map>
+#include<queue>
+#include <limits.h>
+#include<cmath>
+using namespace std;
+
+class Solution
+{
+public:
+int getReqNum(long a,long b,long &n){
+        int gap=0; 
+        while(a <= n){
+            gap += min(n+1,b)-a;
+            a*=10;
+            b*=10;
+        }
+        return gap;
+    }
+    int findKthNumber(long n, int k) {
+        long num = 1;
+        for(int i=1; i<k;){
+            int req = getReqNum(num,num+1,n);
+            if(i+req <= k){
+                i+=req;
+                num++;
+            }else{
+                i++;
+                num *= 10;
+            }
+        }
+        return num;
+    }
+   
+   
+
+};
+
+int main()
+{
+   Solution solution;
+   int ans = solution.findKthNumber(13,2);
+   
+    cout<<ans;
+ 
+    return 0;
+}
